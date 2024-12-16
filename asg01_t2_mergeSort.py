af0f3dcbe7920a30c04bc2e11f06770991c2d162
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-16 09:18:29 +0100

    Update asg01_t2_mergeSort.py
    parse

--- asg01_t2_mergeSort.py
+++ asg01_t2_mergeSort.py
@@ -14,14 +14,8 @@ The function returns a list of integers of the order ids, sorted by t_selection 
 
 
 template:
-
 #!/usr/bin/env python3
-'''
-Std. no: 2711498
-'''
-
 from typing import Optional, List, Tuple, Union
-
 
 class Order:
     def __init__(self, id: int, selection_time: int, shipping_time: int,
@@ -29,13 +23,11 @@ class Order:
         self.id: int = id
         self.selection_time: int = selection_time
         self.shipping_time:  int = shipping_time
-
         '''
         Remove me if you don't need me.
         Or, add a method to assign to me if you want to use linked lists.
         '''
         self.next: Union[Order, None] = next
-
     '''
     Make your life easier and your code prettier, use `Operator Overloading`.
     '''
@@ -44,11 +36,9 @@ class Order:
 def sort(data: List[Tuple[int, int, int]]) -> List[int]:
     '''
     Sort the list of orders by `selection_time + sorting_time`
-
     ### Parameters
     `data`: List of orders represented as tuples
         `[(order_id, selection_time, shipping_time), ...]`
-
     ### Return
     A list of the ids of the sorted orders
     '''
@@ -58,23 +48,19 @@ def sort(data: List[Tuple[int, int, int]]) -> List[int]:
         #         and isinstance(shipping_t, int))
 
         order: Order = Order(id, selection_t, shipping_t)
-
         '''
         TODO: Append the `order` object to your structure.
         '''
         raise NotImplementedError
-
     '''
     TODO: Perform the actual sorting of the orders
     '''
     raise NotImplementedError
-
     '''
     TODO: Return a list of **integers** of the id's (Order.id) of the sorted list.
             i.e: [1, 2, 3, 4, 5, 6]
     '''
     return None
-
 
 '''
 Use for your local testing
@@ -82,11 +68,11 @@ Use for your local testing
 # if __name__ == '__main__':
 #     data = [(1, 500, 100), (2, 700, 100), (3, 100, 100)]
 #     sort(data)
-
-
  
 hints
 1/Consider the format of the data for each task. Ask yourself the following questions by looking at the sample tests and task descriptions.
 • Is the data ordered? partially ordered? or unordered?
 • Does the task require the algorithm to be stable?
 2/Perform floating point operations/comparisons to find the proper ordering.
+
+
